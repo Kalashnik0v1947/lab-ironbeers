5061e35712ba6e9e9e1482849460d7fb1a44359d
Sergey Kadiyevskiy <sergey.kadiyevskiy@gmail.com>
2022-02-01 16:28:56 -0500

    done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+var indexRouter = require('./routes/index');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
+app.use('/', indexRouter);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -21,5 +21,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get("/beers", (req, res, next) => {
+  punkAPI
+    .getBeers().then(beersFromApi => {
+      console.log("Beers from the database:" , beersFromApi);
+      res.render("beers", { beersFromApi: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+app.get("/random-beer", (req, res, next) => {
+  punkAPI
+    .getBeers().then(beersFromApi => {
+      console.log("Beers from the database:" , beersFromApi[Math.floor(Math.random()*beersFromApi.length)]);
+      res.render("random-beer", { beer: beersFromApi[Math.floor(Math.random()*beersFromApi.length)] });
+    })
+    .catch(error => console.log(error));
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
